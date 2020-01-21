c35ddfb939898443405877468285ba338340e982
sanchit <sanchit@tothenew.com>
2020-01-21 13:28:08 +0530

    added running code for hystrix impl

---
+++ spring-hystrix-school-service/src/main/java/com/example/howtodoinjava/springhystrixschoolservice/delegate/StudentServiceDelegate.java
@@ -0,0 +1,52 @@
+package com.example.howtodoinjava.springhystrixschoolservice.delegate;
+
+import java.util.Date;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class StudentServiceDelegate {
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@HystrixCommand(fallbackMethod = "callStudentServiceAndGetData_Fallback", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+	})
+	public String callStudentServiceAndGetData(String schoolname) throws InterruptedException {
+		System.out.println("Getting School details for " + schoolname);
+
+
+//		if(System.currentTimeMillis()  % 2 == 0) {
+//			Thread.sleep(2000);
+//		}
+		String response = restTemplate
+				.exchange("http://localhost:8098/getStudentDetailsForSchool/{schoolname}"
+				, HttpMethod.GET
+				, null
+				, new ParameterizedTypeReference<String>() {
+			}, schoolname).getBody();
+
+		System.out.println("Response Received as " + response + " -  " + new Date());
+
+		return "NORMAL FLOW !!! - School Name -  " + schoolname + " :::  Student Details " + response + " -  " + new Date();
+	}
+	
+	@SuppressWarnings("unused")
+	private String callStudentServiceAndGetData_Fallback(String schoolname) {
+		System.out.println("Student Service is down!!! fallback route enabled...");
+		return "CIRCUIT BREAKER ENABLED!!!No Response From Student Service at this moment. Service will be back shortly - " + new Date();
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
